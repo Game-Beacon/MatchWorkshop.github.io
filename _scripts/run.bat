b5b0686ae08f8c6bb9825231dae4f2fe8366f7b0
JenChieh <jcs090218@gmail.com>
2021-05-09 23:57:31 +0800

    Add run scripts

---
+++ _scripts/run.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2021-05-09 23:55:39 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2021 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+jekyll serve
---
+++ _scripts/run.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+# ========================================================================
+# $File: run.sh $
+# $Date: 2021-05-09 23:57:02 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2021 by Shen, Jen-Chieh $
+# ========================================================================
+
+cd ..
+
+jekyll serve
